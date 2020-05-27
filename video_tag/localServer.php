729ed34d070ad6f7f03f8f15130b7f09d9e09a92
OneCodeMonkey <1460018362@qq.com>
2020-05-27 08:39:25 +0800

    modify

--- video_tag/localServer.php
+++ video_tag/localServer.php
@@ -3,9 +3,11 @@
 $http = new Swoole\Http\Server("0.0.0.0", 9501);
 
 $http->on('request', function ($request, $response) {
-    var_dump($request->get, $request->post);
-    $response->header("Content-Type", "text/html; charset=utf-8");
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+    $username = $request->post["username"];
+    $videoId = $request->post["video_id"];
+    $algo = $request->post["algo"];
+    $response->header("Content-Type", "application/json; charset=utf-8");
+    $response->end(["username" => $username, "video_id" => $video_id, "algo" => $algo]);
 });
 
 $http->start();
