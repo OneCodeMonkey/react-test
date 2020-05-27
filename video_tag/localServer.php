dc77063536b2b725fcad4065fbe12cd7d5c30c19
OneCodeMonkey <1460018362@qq.com>
2020-05-27 08:43:01 +0800

    modify

--- video_tag/localServer.php
+++ video_tag/localServer.php
@@ -6,8 +6,13 @@ $http->on('request', function ($request, $response) {
     $username = $request->post["username"];
     $videoId = $request->post["video_id"];
     $algo = $request->post["algo"];
+
+    $debug = ["username" => $username, "video_id" => $video_id, "algo" => $algo];
+
+    var_dump($debug);
+
     $response->header("Content-Type", "application/json; charset=utf-8");
-    $response->end(["username" => $username, "video_id" => $video_id, "algo" => $algo]);
+    $response->end($debug);
 });
 
 $http->start();
