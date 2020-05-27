42a3240b448c91c2ecd1db4f49e955f4c1282b9a
OneCodeMonkey <1460018362@qq.com>
2020-05-27 15:08:03 +0800

    modify

--- video_tag/getStats.php
+++ video_tag/getStats.php
@@ -16,4 +16,28 @@ foreach ($fileLists as $file) {
     $data = json_decode($content, true);
     $result[] = $data;
 }
-var_dump($result);
+
+$algoStats = [];
+$videoStats = [];
+foreach ($result as $item) {
+    foreach ($item as $videoId => $algo) {
+        if (!empty($algo)) {
+            if (isset($videoStats[$videoId][$algo])) {
+                $videoStats[$videoId][$algo] = intval($videoStats[$videoId][$algo]) + 1;
+            } else {
+                $videoStats[$videoId][$algo] = 1;
+            }
+
+            if (isset($algoStats[$algo]) {
+                $algoStats[$algo] = intval($algoStats[$algo]) + 1;
+            } else {
+                $algoStats[$algo] = 1;
+            }
+        }
+    }
+}
+
+var_dump($algoStats);
+echo "<br>";
+echo "<br>";
+var_dump($videoStats);
