982654e4bf1a88db6ed216cd2f991cdb055a6f49
OneCodeMonkey <1460018362@qq.com>
2020-10-15 18:10:03 +0800

    fix

--- my-app/src/PostList.js
+++ my-app/src/PostList.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import PostItem from "./PostItem";
 
 const data = [
-    {title: "标题1", author: '作者1', date: '2020-10-15 17:00:00'},
-    {title: "标题2", author: '作者2', date: '2020-10-15 17:00:00'},
-    {title: "标题3", author: '作者3', date: '2020-10-15 17:00:00'}
+    {title: "标题1", author: "作者1", date: "2020-10-15 17:00:00"},
+    {title: "标题2", author: "作者2", date: "2020-10-15 17:00:00"},
+    {title: "标题3", author: "作者3", date: "2020-10-15 17:00:00"}
 ];
 
 class PostList extends Component {
